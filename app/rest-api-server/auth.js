ebbe754f98563a78d3055a24ffe18cc132f57938
jimelrod <elrod.development@gmail.com>
2017-01-02 01:12:36 -0600

    Added firebase authentication for all API requests

--- app/rest-api-server/auth.js
+++ app/rest-api-server/auth.js
@@ -1,6 +1,5 @@
 // TODO: Get shit from config...
 var admin = require("firebase-admin");
-
 var serviceAccount = require("C:/keys/nomnomnotebook-firebase-adminsdk-uiiy9-e861d48d8a.json");
 
 admin.initializeApp({
@@ -8,24 +7,30 @@ admin.initializeApp({
     databaseURL: "https://nomnomnotebook.firebaseio.com"
 });
 
-
-
+function parseToken(authHeader) {
+    if (/^Bearer ([^\s\.]+\.){2}[^\s\.]+$/.test(authHeader)) {
+        return authHeader.replace(/^Bearer /, '');;
+    }
+}
 
 module.exports = (req, res, next) => {
-    
-    debugger;
+    let token = parseToken(req.headers.authorization);
+    if (!token) {
+        res.status(401).json("Malformed/Missing Token");
+        return;
+    }
 
-    // admin
-    //     .auth()
-    //     .verifyIdToken(idToken)
-    //     .then(decodedToken => {
-    //         console.log(decodedToken);
-    //         next();
-    //     })
-    //     .catch(error => {
-    //         console.log("ERROR authentication token", error);
-    //     });
-        
-    // TEST ONLY!
-    next();
+    admin
+        .auth()
+        .verifyIdToken(parseToken(req.headers.authorization))
+        .then(decodedToken => {
+            // TODO: Figure out if we can use the decoded token...
+            next();
+        })
+        .catch(error => {
+            // TODO: Log this...
+
+            // TODO: Send 500 or something
+            res.status(401).json("Invalid Token");
+        });
 };
